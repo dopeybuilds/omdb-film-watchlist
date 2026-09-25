b905f15b4e19b14433c3565cbfba15f12c0f3a52
dopeybuilds <vmgvmg123@gmail.com>
2026-09-25 06:43:17 -0400

    redoing the skeleton for movie card, needs CSS before moving onto JS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // API key= http://www.omdbapi.com/?apikey=[784ea01]&s=grave+of+the+fireflies
 
 async function searchMovie() {
-    const res = await fetch("http://www.omdbapi.com/?apikey=[784ea01]&s=grave+of+the+fireflies")
+    const res = await fetch("https://www.omdbapi.com/?apikey=784ea01&s=grave+of+the+fireflies")
     const data = await res.json()
     console.log(data)
 }
