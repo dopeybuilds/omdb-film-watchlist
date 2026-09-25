f384c4eaf77340f06738e04ba48bbcd3fe3890a5
dopeybuilds <vmgvmg123@gmail.com>
2026-09-25 06:43:17 -0400

    more layout and structure work, testing API call for response for movie card layout.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-// API key= http://www.omdbapi.com/?apikey=[784ea01]&s=grave-of-the-fireflies
+// API key= http://www.omdbapi.com/?apikey=[784ea01]&s=grave+of+the+fireflies
 
+async function searchMovie() {
+    const res = await fetch("http://www.omdbapi.com/?apikey=[784ea01]&s=grave+of+the+fireflies")
+    const data = await res.json()
+    console.log(data)
+}
+searchMovie()
